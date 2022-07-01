34449cf9da98b8d8e006d9a249546afcbec0f47f
SoinRoma <soinroma26@gmail.com>
2022-07-01 16:49:42 +0500

    - change structure for project - update README.md

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React, {createContext} from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+
+import './assets/css/bootstrap.min.css';
+
 import UserStore from "./store/UserStore";
 import ContactStore from "./store/ContactStore";
 
