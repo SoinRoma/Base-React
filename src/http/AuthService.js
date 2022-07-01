392bfdb2a412bf715855dd3a7edb33a207bfd2bf
SoinRoma <soinroma26@gmail.com>
2022-07-01 18:00:22 +0500

    - add function for check token

--- src/http/AuthService.js
+++ src/http/AuthService.js
@@ -1,4 +1,5 @@
 import {$host} from './index';
+import axios from "axios";
 
 
 export const login = async (username, password) => {
@@ -7,10 +8,17 @@ export const login = async (username, password) => {
         localStorage.setItem('refresh_token', data.refresh);
         localStorage.setItem('access_token', data.access);
     } catch (e) {
-        alert('Неправильные данные');
+        alert(e);
     }
 }
 
-
+export const checkAuth = async () => {
+    try{
+        const {data} = await axios.get(`${process.env.REACT_APP_API_URL}api/v1/token/`);
+        localStorage.setItem('token', data.token);
+    } catch (e) {
+        alert(e);
+    }
+}
 
 
