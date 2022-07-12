64e52e69ec0460c200e749fd7f6adc2b731db07f
SoinRoma <soinroma26@gmail.com>
2022-07-12 10:28:46 +0500

    - fix function for check token

--- src/App.js
+++ src/App.js
@@ -3,17 +3,26 @@ import AppRouter from "./components/AppRouter";
 import {observer} from "mobx-react-lite";
 import {useContext, useEffect} from "react";
 import {Context} from "./index";
-import {checkAuth} from "./http/AuthService";
+import {checkAuth, logout} from "./http/AuthService";
 
 const App = observer (() => {
 
-    const {auth} = useContext(Context);
+    const {auth, users} = useContext(Context);
 
     useEffect(() => {
         if(localStorage.getItem('refresh_token')){
-            checkAuth().then(() => {
+            checkAuth();
+            if (localStorage.getItem('access_token')){
                 auth.setIsAuth(true);
-            });
+            } else {
+                auth.setIsAuth(false);
+                users.setUsers([]);
+                logout();
+            }
+        } else {
+            auth.setIsAuth(false);
+            users.setUsers([]);
+            logout();
         }
         // eslint-disable-next-line
     }, [])
--- src/http/UsersService.js
+++ src/http/UsersService.js
@@ -1,9 +1,7 @@
 import {$authHost} from './index';
 
-
 export const fetchContacts = async () => {
     const {data} = await $authHost.get('api/v1/twilio/contact/list/');
-    console.log(data.results);
     return data;
 }
 
